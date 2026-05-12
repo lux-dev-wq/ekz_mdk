53a8aac82de45181a74d9fb21edf474775772f53
DE <lux@lux.dev>
2026-05-12 09:21:16 +0300

    first

---
+++ app/src/main/java/com/example/ekz_dmr/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ekz_dmr
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            var text by remember { mutableStateOf("Нажмите кнопку") }
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(text = text)
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(onClick = {
+                    text = Random.nextInt(1, 101).toString()
+                }) {
+                    Text("Сгенерировать")
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Button(onClick = {
+                    text = "Нажмите кнопку"
+                }) {
+                    Text("Сброс")
+                }
+            }
+        }
+    }
+}
